85e1f9313ab8b6e65fe8e536bc1671a2522e4d25
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-11-08 12:18:30 +0700

    mongoose connect & nodemon

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/CarVirDe");
+
 app.use(bodyParser.json());
 
 app.use((err, req, res, next) => {
