64a1611da4df4f95a648db7f6d2caa965d7d2f27
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-11-15 13:31:42 +0700

    structure refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import "semantic-ui/dist/semantic.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+
+// import reducers from "./reducers/index";
+// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+l;
